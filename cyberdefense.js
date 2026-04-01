215703e38f8603fde0cbf369057d5f14f6e924d3
JDi3on2004 <jdizon2204@gmail.com>
2026-04-01 13:44:04 -0400

    Update redirect path for vocabblast game

--- cyberdefense.js
+++ cyberdefense.js
@@ -1087,7 +1087,7 @@ function endGame() {
 function goNext() {
   const passed = localStorage.getItem('game2_passed') === 'true';
   if (passed) {
-    window.location.href = '../games/vocabblast.html';
+    window.location.href = 'vocabblast.html';
   } else {
     showToast('💡 Survive all 5 waves to unlock Game 3!');
   }
